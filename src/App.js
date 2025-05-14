bab298619696865032415e1d85445137c396ba50
karen <karen.garzon215@pascualbravo.edu.co>
2025-05-13 20:45:18 -0500

    cambie la letra del HEADER por segunda vez

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { Routes, Route,BrowserRouter } from 'react-router-dom';
 import "bootstrap-icons/font/bootstrap-icons.css";
-<<<<<<< HEAD
-import ShowUsers from './components/AllUsers';
-import "bootstrap-icons/font/bootstrap-icons.css";
-=======
 import Showusers from './components/showUsers/ShowUsers';
 import Header from './components/header/Header';
->>>>>>> 1bd6cee069a716aa41ff5c1e89c1f146803e1f06
 
 function App() {
   return (
